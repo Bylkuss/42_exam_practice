ddaec1dc7b3acd6cd1db446d775af1cca78d49a1
Lounes Adjou <lmatoub91@gmail.com>
2023-01-20 12:31:40 -0500

    simplified gnl for practice for exam rank03

--- Exam_rank03/get_next_line/get_next_line.c
+++ Exam_rank03/get_next_line/get_next_line.c
@@ -6,8 +6,49 @@
 /*   By: loadjou <loadjou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 18:20:17 by loadjou           #+#    #+#             */
-/*   Updated: 2023/01/16 18:20:32 by loadjou          ###   ########.fr       */
+/*   Updated: 2023/01/20 12:31:16 by loadjou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *get_next_line(int fd);
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+    char c;
+    int i = 0;
+    int rd;
+    char *buff = malloc(sizeof(BUFFER_SIZE) * 10000);
+    while ((rd = read(fd, &c, BUFFER_SIZE)) > 0)
+    {
+        buff[i++] = c;
+        if (c == '\n')
+            break;
+    }
+    if ((!buff[i - 1] && !rd) || rd == -1)
+    {
+        free(buff);
+        return 0;
+    }
+    buff[i] = '\0';
+    return buff;
+}
+
+/*
+int main(void)
+{
+    char *line;
+    int i;
+    int fd1;
+    fd1 = open("subject.en.txt", O_RDONLY);
+    i = 1;
+    line = get_next_line(fd1);
+    while (line)
+    {
+        printf("%s", line);
+        line = get_next_line(fd1);
+        free(line);
+    }
+    close(fd1);
+    return (0);
+}
+*/
---
+++ Exam_rank03/get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loadjou <loadjou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 11:38:18 by loadjou           #+#    #+#             */
+/*   Updated: 2023/01/20 11:57:13 by loadjou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#undef BUFFER_SIZE
+#define BUFFER_SIZE 1
+
+#endif
